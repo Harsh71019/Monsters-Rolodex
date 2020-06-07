672e504ca62eb0a4bba4778a4725086edd12bda8
harsh <harsh710pat@gmail.com>
2020-06-07 16:58:28 +0530

    Updates

---
+++ precache-manifest.f803026518bf92061a2b54c8b9159ce1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80dfaf21100af24187c82b9a04c60d33",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "9c8f80d5a7d38b5213e2",
+    "url": "/Monsters-Rolodex/static/css/main.3ae7227f.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/Monsters-Rolodex/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Monsters-Rolodex/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c8f80d5a7d38b5213e2",
+    "url": "/Monsters-Rolodex/static/js/main.8d090708.chunk.js"
+  },
+  {
+    "revision": "bddd2872dd5650c52730",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.dc822ace.js"
+  }
+]);
